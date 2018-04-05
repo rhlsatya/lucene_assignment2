cda237b57dec65b35c703f58fe9dbe747b52c225
satyar@tcd.ie <rahulsatya@10.6.42.157>
2018-04-05 17:05:49 +0100

    asd

--- lucene_assignment2/src/lucene_assignment2/SearcherFr94.java
+++ lucene_assignment2/src/lucene_assignment2/SearcherFr94.java
@@ -91,17 +91,17 @@ public class SearcherFr94 {
 	
 	public void addQuery(String query, int flag) throws ParseException
 	{
-		QueryParser parser1 = new QueryParser("usDept", analyzer);
-		QueryParser parser2 = new QueryParser("agency", analyzer);
-		QueryParser parser3 = new QueryParser("usBureau", analyzer);
+//		QueryParser parser1 = new QueryParser("usDept", analyzer);
+//		QueryParser parser2 = new QueryParser("agency", analyzer);
+//		QueryParser parser3 = new QueryParser("usBureau", analyzer);
 		QueryParser parser4 = new QueryParser("docTitle", analyzer);
 		QueryParser parser5 = new QueryParser("summary", analyzer);
 		QueryParser parser6 = new QueryParser("supplem", analyzer);
 		QueryParser parser7 = new QueryParser("textother", analyzer);
 		    
-		Query query1 = parser1.parse(query);
-		Query query2 = parser2.parse(query);
-		Query query3 = parser3.parse(query);
+//		Query query1 = parser1.parse(query);
+//		Query query2 = parser2.parse(query);
+//		Query query3 = parser3.parse(query);
 		Query query4 = parser4.parse(query);
 		Query query5 = parser5.parse(query); // must
 		Query query6 = parser6.parse(query);
@@ -109,18 +109,18 @@ public class SearcherFr94 {
 		
 		
 		
-	    Query boostedTermQuery1 = new BoostQuery(query1, (float) 1.5);
-	    Query boostedTermQuery2 = new BoostQuery(query2, 2);
-	    Query boostedTermQuery3 = new BoostQuery(query3, (float) 2.5);
+//	    Query boostedTermQuery1 = new BoostQuery(query1, (float) 1.5);
+//	    Query boostedTermQuery2 = new BoostQuery(query2, 2);
+//	    Query boostedTermQuery3 = new BoostQuery(query3, (float) 2.5);
 	    Query boostedTermQuery4 = new BoostQuery(query4, (float) 6.5);
 	    Query boostedTermQuery5 = new BoostQuery(query5, 2);
 	    Query boostedTermQuery6 = new BoostQuery(query6, (float) 1.5);
 	    Query boostedTermQuery7 = new BoostQuery(query7, (float) 0.7);
 	    
 	    
-	    booleanQuery.add(boostedTermQuery1, Occur.SHOULD);
-	    booleanQuery.add(boostedTermQuery2, Occur.SHOULD);
-	    booleanQuery.add(boostedTermQuery3, Occur.SHOULD);
+//	    booleanQuery.add(boostedTermQuery1, Occur.SHOULD);
+//	    booleanQuery.add(boostedTermQuery2, Occur.SHOULD);
+//	    booleanQuery.add(boostedTermQuery3, Occur.SHOULD);
 	    booleanQuery.add(boostedTermQuery4, Occur.SHOULD);
 	    booleanQuery.add(boostedTermQuery6, Occur.SHOULD);
 	    booleanQuery.add(boostedTermQuery7, Occur.SHOULD);

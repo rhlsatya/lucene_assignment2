37df752648fc8add790d7a6e58065184ea679d1d
satyar@tcd.ie <rahulsatya@10.6.42.157>
2018-04-05 21:40:19 +0100

    asd

--- lucene_assignment2/src/lucene_assignment2/SearcherFbis.java
+++ lucene_assignment2/src/lucene_assignment2/SearcherFbis.java
@@ -65,7 +65,7 @@ public class SearcherFbis {
 //        boostFields.put("abs",5f);
 //        boostFields.put("date",2f);
 //        boostFields.put("fcontent",2f);
-        boostFields.put("textcontent",55f);
+        boostFields.put("textcontent",255f);
         //"abs","date","fcontent"
         //.1114
         // .1117 is without the similarity for fr94

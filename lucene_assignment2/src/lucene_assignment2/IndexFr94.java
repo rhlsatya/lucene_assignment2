3d7f0bf25d23af40db57618868b720858e3be7ae
satyar@tcd.ie <rahulsatya@10.6.42.157>
2018-04-05 16:45:13 +0100

    asd

--- lucene_assignment2/src/lucene_assignment2/IndexFr94.java
+++ lucene_assignment2/src/lucene_assignment2/IndexFr94.java
@@ -9,6 +9,7 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Paths;
 
 import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.en.EnglishAnalyzer;
 import org.apache.lucene.analysis.standard.StandardAnalyzer;
 import org.apache.lucene.document.Document;
 import org.apache.lucene.document.Field;
@@ -67,7 +68,7 @@ public class IndexFr94 {
 	{
 		String indexPath = "/Users/rahulsatya/Desktop/IndexedFiles/fr94/";
 		Directory dir = FSDirectory.open(Paths.get(indexPath));
-		Analyzer analyzer = new StandardAnalyzer();
+		Analyzer analyzer = new EnglishAnalyzer();
 		IndexWriterConfig iwc = new IndexWriterConfig(analyzer);
 		IndexWriter writer = new IndexWriter(dir, iwc);
 		Document doc = new Document();

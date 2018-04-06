c57d846d267ca6d7086217eab70ab2705bdd6bb1
satyar@tcd.ie <rahulsatya@10.6.42.157>
2018-04-06 02:39:12 +0100

    asd

--- lucene_assignment2/src/lucene_assignment2/SearcherFbis.java
+++ lucene_assignment2/src/lucene_assignment2/SearcherFbis.java
@@ -62,7 +62,7 @@ public class SearcherFbis {
 	    
 	    Map<String, Float> boostFields = new HashMap<String, Float>();
         boostFields.put("heading",10f);
-        boostFields.put("abs",10f);
+        boostFields.put("abs",20f);
 //        boostFields.put("date",2f);
        
         boostFields.put("textcontent",245f);

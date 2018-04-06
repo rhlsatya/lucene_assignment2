81ef4847b01ea2f8caf7f68b1788c18d1ee70564
satyar@tcd.ie <rahulsatya@10.6.42.157>
2018-04-06 01:09:08 +0100

    asd

--- lucene_assignment2/src/lucene_assignment2/SearcherLatimes.java
+++ lucene_assignment2/src/lucene_assignment2/SearcherLatimes.java
@@ -66,8 +66,8 @@ public class SearcherLatimes {
         boostFields.put("heading",20f);
 //        boostFields.put("abs",5f);
 //        boostFields.put("date",2f);
-        boostFields.put("subject",50f);
-        boostFields.put("textcontent",275f);
+        boostFields.put("subject",150f);
+        boostFields.put("textcontent",255f);
         //"abs","date","fcontent"
         //.1114
         // .1117 is without the similarity for fr94

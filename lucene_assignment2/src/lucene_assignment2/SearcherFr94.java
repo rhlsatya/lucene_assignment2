e534d73b1524a538d64e774f1e87bceffd38ca2d
satyar@tcd.ie <rahulsatya@10.6.42.157>
2018-04-06 13:20:31 +0100

    asd

--- lucene_assignment2/src/lucene_assignment2/SearcherFr94.java
+++ lucene_assignment2/src/lucene_assignment2/SearcherFr94.java
@@ -57,13 +57,13 @@ public class SearcherFr94 {
 	//as
 	    Map<String, Float> boostFields = new HashMap<String, Float>();
         boostFields.put("doctitle",25f);
-        boostFields.put("summary",175f);
+        boostFields.put("summary",75f);
         boostFields.put("supplem",10f);
-        boostFields.put("textother",25f);
+        boostFields.put("textother",125f);
         
         //java lucene_assignment2.BuildQuery
 	    //javac SearcherFr94.java SearcherFbis.java SearcherLatimes.java SearcherFt.java BuildQuery.java
-        //./trec_eval/trec_eval qrels.assignment2.part1 results.txt
+        //./trec_eval/trec_eval qrels.assignment2 results.txt
     
         MultiFieldQueryParser parser = new MultiFieldQueryParser(new String[]{"doctitle","summary", "textother","supplem"}, analyzer, boostFields);
         parser.setAllowLeadingWildcard(true);
